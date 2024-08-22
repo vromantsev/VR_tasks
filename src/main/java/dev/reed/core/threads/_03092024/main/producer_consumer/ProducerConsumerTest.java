3bc7d004f2cc1687bb703cda325e57ad68949776
vromantsev <vladyslav.romantsev@gmail.com>
2024-08-22 18:21:49 +0300

    renamed main class

--- src/main/java/dev/reed/core/threads/_03092024/main/producer_consumer/ProducerConsumerTest.java
+++ src/main/java/dev/reed/core/threads/_03092024/main/producer_consumer/ProducerConsumerTest.java
@@ -3,7 +3,7 @@ package dev.reed.core.threads._03092024.main.producer_consumer;
 /**
  * Please refer to the task description - {@link  dev.reed.core.threads._03092024.main.producer_consumer.task.md}
  */
-public class SupplyChain {
+public class ProducerConsumerTest {
 
     public static void main(String[] args) throws InterruptedException {
         // TODO test your code here
