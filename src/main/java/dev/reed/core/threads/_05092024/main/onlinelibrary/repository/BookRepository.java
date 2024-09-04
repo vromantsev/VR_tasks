b1defe082865a73eec3ec5c15571c9fe3c74d387
vromantsev <vladyslav.romantsev@gmail.com>
2024-09-04 23:16:44 +0300

    added classes

---
+++ src/main/java/dev/reed/core/threads/_05092024/main/onlinelibrary/repository/BookRepository.java
@@ -0,0 +1,58 @@
+package dev.reed.core.threads._05092024.main.onlinelibrary.repository;
+
+import dev.reed.core.threads._05092024.main.onlinelibrary.entity.Book;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+public class BookRepository {
+
+    private final ConcurrentMap<Long, Book> bookIdToBookMap = new ConcurrentHashMap<>();
+
+    public void save(final Book book) {
+        long bookId = generateId();
+        book.setId(bookId);
+        bookIdToBookMap.put(bookId, book);
+    }
+
+    public Optional<Book> getById(final Long bookId) {
+        return Optional.ofNullable(bookIdToBookMap.get(bookId));
+    }
+
+    public boolean isAvailable(final Long bookId) {
+        return bookIdToBookMap.containsKey(bookId);
+    }
+
+    public Book update(final Book book) {
+        return Optional.ofNullable(bookIdToBookMap.get(book.getId()))
+                .map(current -> updateBookFields(book))
+                .orElseThrow();
+    }
+
+    public boolean deleteById(final Long bookId) {
+        return this.bookIdToBookMap.remove(bookId) != null;
+    }
+
+    public Collection<Book> getAll() {
+        return this.bookIdToBookMap.values();
+    }
+
+    private static Book updateBookFields(final Book book) {
+        Function<Book, Book> updateBookFieldsFunction = current -> {
+            current.setTitle(book.getTitle());
+            current.setAuthor(book.getAuthor());
+            current.setAvailable(book.isAvailable());
+            return current;
+        };
+        return updateBookFieldsFunction.apply(book);
+    }
+
+    private static long generateId() {
+        return ThreadLocalRandom.current().nextLong(1, 100_000_000);
+    }
+}
